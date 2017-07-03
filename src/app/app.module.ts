d6a88e181555441dac3448f5807628b09f3899d4
firstgit <505257618@qq.com>
2017-07-03 15:35:27 +0800

    重头

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeroFormComponent } from './hero-form.component';
+// import { HeroFormComponent } from './hero-form.component';
+// import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroFormComponent
+    // HeroFormComponent,
+    // LoginComponent
   ],
   imports: [
     BrowserModule,
